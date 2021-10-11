bdea7a58311fadc568fdeb249c127ce87896ec00
ramdhanidesu <ramdhaniaza09@gmail.com>
2021-10-11 12:49:17 +0700

    UTS-PRAKTIK-PBO

---
+++ Views/welcome.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      header img{
+        height:30px;
+        margin-top:6px;
+        margin-right:10px;
+      }
+
+    </style>
+    <title>Document</title>
+</head>
+
+<body style="background:url('https://1.bp.blogspot.com/-DuRtgmaUhIk/YGFXVkyq3DI/AAAAAAAAIMQ/i80mKqCTfesgRpUd1V7QPmkJ6DEahRWLwCLcBGAsYHQ/s1920/biru-pastel.jpg');">
+<div class="container py-4">
+    <header class="pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2" viewBox="0 0 118 94" role="img">
+        <title>Bootstrap</title>
+        <img src="https://tarunakelapa.files.wordpress.com/2016/02/logo-smkn4.png"></svg>
+        <span class="fs-4">SMK NEGERI 4 BOGOR</span>
+      </a>
+    </header>
+
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">SMK Negeri 4 Kota Bogor (SMK NEBRAZKA)</h1>
+        <p class="col-md-8 fs-4">"SMK Negeri 4 Bogor dikenal juga dengan sebutan NEBRAZKA(mirip Nebraska negara bagian Amerika) singkatan dari Negeri Empat Bogor AZKA, azka sendiri memiliki arti suci, berbudi luhur."</p>
+      </div>
+    </div>
+
+    <div class="row align-items-md-stretch">
+      <div class="col-md-6">
+        <div class="h-10 p-5 text-white bg-dark rounded-3">
+          <h2>All The Content</h2>
+          <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+        </div>
+      </div>
+      
+      <div class="col-md-6">
+        <div class="h-10 p-5 bg-light border rounded-3">
+          <h2>Alamat</h2>
+          <p>Jl. Raya Tajur, Kp. Buntar RT.02/RW.08, Kel. Muara sari, Kec. Bogor Selatan, RT.03/RW.08, Muarasari, Kec. Bogor Sel., Kota Bogor, Jawa Barat 16137</p>
+        </div>
+      </div>
+    </div>
+
+   
+  </div>
+</body>
+</html>
